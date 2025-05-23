8f0e4fb71d1a409233565b7b4fb4d71f9b9d58e2
강진주 <rkdwlswn227@gmail.com>
2025-05-23 19:44:17 +0900

    🎉init: init project

---
+++ coffeechat/src/main/java/com/example/coffeechat/CoffeechatApplication.java
@@ -0,0 +1,13 @@
+package com.example.coffeechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeechatApplication.class, args);
+    }
+
+}
---
+++ coffeechat/src/test/java/com/example/coffeechat/CoffeechatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coffeechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeechatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
